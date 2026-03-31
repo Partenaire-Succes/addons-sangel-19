6844025575800dfb0c119274e618260e190ef84e
adams0008 <adama.madjib@gmail.com>
2026-03-31 18:17:30 +0000

    Mise a jours des import limit de credit

--- custom_api_sage_x3/models/create_contact_sage.py
+++ custom_api_sage_x3/models/create_contact_sage.py
@@ -213,6 +213,7 @@ class ResPartnerImport(models.Model):
         vals = {
             "name":               name,
             "customer_id":        customer_code,
+            "barcode":            customer_code,
             "is_company":         is_company,
             "customer_rank":      1,
             "street":             self._safe_string(customer.get("bpaadD_0")),
