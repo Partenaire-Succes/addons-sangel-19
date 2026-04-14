02446945b01941cd04bf471f4f1b5c0d47cf256e
adams0008 <adama.madjib@gmail.com>
2026-04-14 11:19:52 +0000

    Merge Marcel_dev

--- custom_api_sage_x3/models/create_contact_sage.py
+++ custom_api_sage_x3/models/create_contact_sage.py
@@ -102,7 +102,7 @@ class ResPartnerImport(models.Model):
                     )
 
                     if existing:
-                        existing.write(vals)
+                        # existing.write(vals)
                         existing.name = vals["name"]  # Forcer la mise à jour du nom dans le cache d'Odoo
                         _logger.info("🔄 Mis à jour : %s (%s)", existing.name, existing.customer_id)
                         existing.write({'customer_account': vals.get("customer_account", existing.customer_account)})
@@ -206,8 +206,8 @@ class ResPartnerImport(models.Model):
             raise ValueError("Code client manquant")
 
         name        = self._safe_string(customer.get("bprnaM_0"))
-        if not name:
-            raise ValueError(f"Nom manquant pour client {customer_code}")
+        # if not name:
+        #     raise ValueError(f"Nom manquant pour client {customer_code}")
         
         is_company  = bool(self._safe_string(customer.get("crN_0")))
         vat_regime  = self._safe_string(customer.get("vacbpR_0"))
